cca9ad665ea84c94a2a4b40665d5e65a71880d00
Loohit <uppuloohit@gmail.com>
2026-04-19 19:09:59 +0530

    LockTable implemented

--- tl2/hash_table.h
+++ tl2/hash_table.h
@@ -1,6 +1,24 @@
 #pragma once
-#include <unordered_map>
+#include <vector>
 #include "version_lock.h"
 #include "types.h"
 
-static std::unordered_map<uint *, VersionLock> hashtbl;
+constexpr size_t LOCKTABLE_SIZE = 1<<20;
+
+struct LockTable{
+private:
+    std::vector<VersionLock> table;
+    size_t mask;
+
+public:
+    explicit LockTable(size_t size_pow2) : table(size_pow2) , mask(size_pow2) {}
+
+    //maps address to version lock (hashing)
+    inline VersionLock& operator[] (uint32_t* addr) {
+        uintptr_t x = reinterpret_cast<uintptr_t>(addr);
+        size_t index = (x>>3) & mask;
+        return table[index];
+    }
+};
+
+LockTable hashtbl(LOCKTABLE_SIZE);
