7677c644d651c3ca27227c3590da476141917978
Dhruv-Prasad <dhruvpras@gmail.com>
2026-04-26 00:29:39 +0530

    LogResourceHandle

--- tl2/memory.h
+++ tl2/memory.h
@@ -25,13 +25,22 @@ namespace tl2::internal {
 		return allocation_strategy.load(std::memory_order_acquire);
 	}
 
-	inline std::unique_ptr<std::pmr::monotonic_buffer_resource>	make_transaction_resource(std::byte *local_buffer, const std::size_t local_buffer_size) {
-		if (get_allocation_strategy() == AllocationStrategy::SynchronizedPool) {
-			// This is weird to do, but I only need to change one line here for testing
-			return std::make_unique<std::pmr::monotonic_buffer_resource>(&synchronized_pool_resource);
-		}
-		else {
-			return std::make_unique<std::pmr::monotonic_buffer_resource>(local_buffer, local_buffer_size);
+	struct LogResourceHandle {
+		std::pmr::memory_resource *resource{nullptr};
+		std::unique_ptr<std::pmr::monotonic_buffer_resource> owned_resource{};
+	};
+
+	template <AllocationStrategy AS>
+	inline LogResourceHandle make_log_resource(std::byte *local_buffer, const std::size_t local_buffer_size) {
+		if constexpr (AS == AllocationStrategy::SynchronizedPool) {
+			return LogResourceHandle{&synchronized_pool_resource, nullptr};
+		} else {
+			auto owned = std::make_unique<std::pmr::monotonic_buffer_resource>(
+				local_buffer, local_buffer_size, std::pmr::null_memory_resource());
+			LogResourceHandle handle;
+			handle.resource = owned.get();
+			handle.owned_resource = std::move(owned);
+			return handle;
 		}
 	}
 
