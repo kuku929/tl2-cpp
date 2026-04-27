95f9657fe9fb0ebe93434204e17ef417dcad52c1
kuku929 <krutarthpatel929@gmail.com>
2026-04-27 17:00:53 +0530

    weird issue happened

--- tl2/memory.h
+++ tl2/memory.h
@@ -3,9 +3,7 @@
 #include <memory_resource>
 
 namespace tl2::internal {
-
 	// Single shared synchronized pool resource for those logs that are created
 	// with an external resource (e.g., to share allocations across threads).
 	inline std::pmr::synchronized_pool_resource synchronized_pool_resource{};
-
 } // namespace tl2::internal
