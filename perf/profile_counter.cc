658a8e6ed9144ba1b76b33d079a60bf3ee8a43fc
kuku929 <krutarthpatel929@gmail.com>
2026-04-28 23:35:05 +0530

    added performance environment

---
+++ perf/profile_counter.cc
@@ -0,0 +1,39 @@
+#include "tl2/tl2.h"
+#include "profile.h"
+#include <iostream>
+#include <thread>
+using namespace tl2;
+
+uint run(const uint niters) {
+  TVar<uint> counter(0);
+  std::thread t1([&]() {
+    for (uint _ = 0; _ < niters; ++_) {
+      atomically([&]() {
+        uint val = static_cast<uint>(counter);
+        counter = val + 1;
+      });
+    }
+  });
+  std::thread t2([&]() {
+    for (uint _ = 0; _ < niters; ++_) {
+      atomically([&]() {
+        uint val = static_cast<uint>(counter);
+        counter = val + 1;
+      });
+    }
+  });
+  t1.join();
+  t2.join();
+  uint ans = 0;
+  {
+    atomically([&]() { ans = static_cast<uint>(counter); });
+  }
+  return ans;
+}
+
+int main() {
+  for (uint i = 1; i < 100'000; i += 10'000) {
+    run(i);
+  }
+  return 0;
+}
